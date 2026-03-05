187e63d1092950d57a66d2e6896a8874131e0432
SvartNewSki <makhmudovAmir@gmail.com>
2026-03-05 16:02:39 +0300

    add routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/main', [WebController::class, 'index']);
+
+Route::get('/register', function(){
+    return view('register');
+});
+
+Route::get('/auth', function(){
+   return view('movies');
+});
+
+Route::get('/movies/movie_page',function (){
+    return view('movie_page');
 });
