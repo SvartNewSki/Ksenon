df488165d00a3e1fb4774338bbf76c05a8360aed
SvartNewSki <makhmudovAmir@gmail.com>
2026-03-12 12:07:05 +0300

    fix routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,22 +1,17 @@
 <?php
 
+use App\Http\Controllers\ContentController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/main', [WebController::class, 'index'])->name('main');
+Route::get('/register', [UserController::class, 'register'])->name('register');
+Route::get('/auth', [UserController::class, 'auth'])->name('auth');
+Route::get("/post", [PostController::class, 'index']);
+Route::get("/movie", [ContentController::class, 'movie']);
 
-Route::get('/main', [WebController::class, 'index']);
-
-Route::get('/register', function(){
-    return view('register');
-});
-
-Route::get('/auth', function(){
-   return view('movies');
-});
-
-Route::get('/movies/movie_page',function (){
-    return view('movie_page');
-});
