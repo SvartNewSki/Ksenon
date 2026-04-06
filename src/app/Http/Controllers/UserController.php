445e9a74913698b7b7f2b4ebabc0f259ac24d8c5
SvartNewSki <makhmudovAmir@gmail.com>
2026-04-06 10:41:40 +0300

    fix controllers

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller{
         return view('auth');
     }
 
-    public function register(){
-        return view ('register');
+    public function reg(){
+        return view ('reg');
     }
 }
