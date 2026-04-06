4516b3b55e1bc9e99f84fa6711f96f0cef032698
SvartNewSki <makhmudovAmir@gmail.com>
2026-04-06 10:40:05 +0300

    remove blade php

--- src/resources/views/film.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-@section('content')
-     @foreach ($movie as $movie)
-        <div>{{$movie->title}}</div>
-    @endforeach
-    film page
-@endsection
-   
--- src/resources/views/register.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main')
-@section('content')
-     @foreach ($post as $movie)
-        <div>{{$movie->title}}</div>
-    @endforeach
-    Register page
-@endsection
